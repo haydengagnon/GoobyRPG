f92dd2d8e74310453a1f8acde093ab3116211c98
Hayden Gagnon <haydengagnon1@gmail.com>
2024-01-03 17:00:09 -0600

    village layout and world signs

---
+++ scripts/village.gd
@@ -0,0 +1,56 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if Global.world_to_village == true:
+		$player.position.x = Global.player_world_to_village_posx
+		$player.position.y = Global.player_world_to_village_posy
+		Global.world_to_village = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	change_scene()
+	set_camera_limits()
+
+
+
+		
+func set_camera_limits():
+	if Global.current_scene == "village":
+		$player/Camera2D.limit_left = 0
+		$player/Camera2D.limit_right = 1120
+		$player/Camera2D.limit_top = 0
+		$player/Camera2D.limit_bottom = 640
+		
+func change_scene():
+	if Global.transition_scene == true:
+		if Global.world_enter == true:
+			get_tree().change_scene_to_file("res://scenes/world.tscn")
+			Global.finish_changescenes()
+			Global.village_to_world = true
+
+func _on_world_entrance_body_entered(body):
+	if body.has_method("player"):
+		Global.world_enter = true
+		Global.transition_scene = true
+
+func _on_world_entrance_body_exited(body):
+	if body.has_method("player"):
+		Global.transition_scene = false
+
+
+func _on_north_entrance_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_north_entrance_body_exited(body):
+	pass # Replace with function body.
+
+
+func _on_west_entrance_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_west_entrance_body_exited(body):
+	pass # Replace with function body.
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var start_area_sign = false
+var start_sign_animation_done = false
+var cliffside_area_sign = false
+var cliffside_sign_animation_done = false
+
 func _ready():
 	if Global.start_to_world == true:
 		$player.position.x = Global.player_start_to_world_posx
@@ -9,10 +14,16 @@ func _ready():
 		$player.position.x = Global.player_exit_cliffside_posx
 		$player.position.y = Global.player_exit_cliffside_posy
 		Global.cliffside_to_world = false
+	elif Global.village_to_world == true:
+		$player.position.x = Global.player_village_to_world_posx
+		$player.position.y = Global.player_village_to_world_posy
+		Global.village_to_world = false
+	Global.current_scene = "world"
 
 func _process(_delta):
 	change_scene()
 	set_camera_limits()
+	display_sign_text()
 
 func _on_cliffside_entrance_body_entered(body):
 	if body.has_method("player"):
@@ -43,6 +54,10 @@ func change_scene():
 			get_tree().change_scene_to_file("res://scenes/cliffside.tscn")
 			Global.finish_changescenes()
 			Global.world_to_cliffside = true
+		elif Global.village_enter == true:
+			get_tree().change_scene_to_file("res://scenes/village.tscn")
+			Global.finish_changescenes()
+			Global.world_to_village = true
 
 func set_camera_limits():
 	if Global.current_scene == "world":
@@ -50,6 +65,60 @@ func set_camera_limits():
 		$player/Camera2D.limit_right = 416
 		$player/Camera2D.limit_top = 0
 		$player/Camera2D.limit_bottom = 191
+
+
+func _on_village_entrance_body_entered(body):
+	if body.has_method("player"):
+		Global.village_enter = true
+		Global.transition_scene = true
+
+
+func _on_village_entrance_body_exited(body):
+	if body.has_method("player"):
+		Global.transition_scene = false
+
+
+func _on_start_sign_body_entered(body):
+	if body.has_method("player"):
+		start_area_sign = true
+
+func _on_start_sign_body_exited(body):
+	if body.has_method("player"):
+		start_area_sign = false
+		
+func _on_cliffside_sign_body_entered(body):
+	if body.has_method("player"):
+		cliffside_area_sign = true
+
+
+func _on_cliffside_sign_body_exited(body):
+	if body.has_method("player"):
+		cliffside_area_sign = false
+	
+	
+func display_sign_text():
+	if start_area_sign == true:
+		$AnimationPlayer/start_sign_east.visible = true
+		if start_sign_animation_done == false:
+			$AnimationPlayer.play("startsign")
+	elif start_area_sign == false:
+		$AnimationPlayer/start_sign_east.visible = false
+		start_sign_animation_done = false
+	
+	if cliffside_area_sign == true:
+		$AnimationPlayer/cliffside_sign_text.visible = true
+		if cliffside_sign_animation_done == false:
+			$AnimationPlayer.play("cliffsidesign")
+	elif cliffside_area_sign == false:
+		$AnimationPlayer/cliffside_sign_text.visible = false
+		cliffside_sign_animation_done = false
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "startsign":
+		start_sign_animation_done = true
+	if anim_name == "cliffsidesign":
+		cliffside_sign_animation_done = true
 
 
 
