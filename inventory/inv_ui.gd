0ed0af7cae3ceb330c110d8ba325ca79f5111fde
Hayden Gagnon <haydengagnon1@gmail.com>
2024-01-11 12:29:44 -0600

    inventory sprites

--- inventory/inv_ui.gd
+++ inventory/inv_ui.gd
@@ -1,10 +1,18 @@
 extends Control
 
+@onready var inv: Inv = preload("res://inventory/playerinv.tres")
+@onready var slots: Array = $NinePatchRect/GridContainer.get_children()
+
 var is_open = false
 
 func _ready():
+	update_slots()
 	close()
-	
+
+func update_slots():
+	for i in range(min(inv.equipment.size(), slots.size())):
+		slots[i].update(inv.equipment[i])
+
 func _process(_delta):
 	if Input.is_action_just_pressed("inventory"):
 		if is_open == true:
